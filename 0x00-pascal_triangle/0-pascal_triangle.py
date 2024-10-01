7c947d2e33f69c6f87428db0897451d4a4c2de81
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-01 04:38:19 +0100

    first commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    result = []
+    for i in range(n):
+        temp = []
+        for j in range(i):
+            if j == 0:
+                temp.append(1)
+            else:
+                temp.append(result[-1][j - 1] + result[-1][j])
+        temp.append(1)
+        result.append(temp)
+    return result
