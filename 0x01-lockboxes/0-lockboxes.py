9a682a735278f4eb5191a69fa0e9f9bca9e92218
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-08 08:51:36 +0100

    Completed lockboxes challenge

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    visitedBoxes = set()
+    visitBox(0, boxes, visitedBoxes)
+    return len(visitedBoxes) == len(boxes)
+
+
+def visitBox(boxIndex, boxes, visitedBoxes):
+    keys = boxes[boxIndex]
+    visitedBoxes.add(boxIndex)
+    if len(keys) == 0:
+        return
+    for key in keys:
+        if key not in visitedBoxes:
+            visitBox(key, boxes, visitedBoxes)
