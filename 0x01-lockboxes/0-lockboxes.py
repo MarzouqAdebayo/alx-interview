a4663b3470f5d32c6520260a3f29ef1aa93a7087
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-08 09:04:12 +0100

    Added edge case for an empty box

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,16 +5,17 @@ Module '0-lockboxes.py'
 
 
 def canUnlockAll(boxes):
-    visitedBoxes = set()
+    if len(boxes) == 0:
+        return False
+    visitedBoxes = []
     visitBox(0, boxes, visitedBoxes)
+    print(visitedBoxes, boxes)
     return len(visitedBoxes) == len(boxes)
 
 
 def visitBox(boxIndex, boxes, visitedBoxes):
     keys = boxes[boxIndex]
-    visitedBoxes.add(boxIndex)
-    if len(keys) == 0:
-        return
+    visitedBoxes.append(boxIndex)
     for key in keys:
         if key not in visitedBoxes:
             visitBox(key, boxes, visitedBoxes)
