0a48a65dae19c9271154b568063b608e6481357c
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-10-21 07:45:11 +0100

    Commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Module '0-minoperations.py' contains minoperations"""
+
+
+def minOperations(n: int):
+    """Computes the fewest number of operations needed to give exactly
+    n 'H' characters"""
+    if not isinstance(n, int):
+        return 0
+    operations = 0
+    divisor = 2
+
+    while n > 1:
+        while n % divisor == 0:
+            operations += divisor
+            n //= divisor
+        divisor += 1
+    return operations
