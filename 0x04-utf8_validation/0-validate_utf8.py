460b8834d943ad5a058c8091f5761c0c37f80baf
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-02 12:47:59 +0100

    :wrench: Fixed bugs in 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -2,48 +2,53 @@
 """Module '0-validate_utf8.py' """
 
 
-def get_number_of_bytes(first_byte):
+def extract_and_count_bits(first_byte):
     """Gets the number of bytes on based on the
-    starting bit in arg first_byte"""
+    starting bit in arg first_byte and
+    extract the rest of the bits"""
     one_byte_mask = 0x80  # 0b10000000
     two_byte_mask = 0xC0  # 0b11000000
     three_byte_mask = 0xE0  # 0b11100000
     four_byte_mask = 0xF0  # 0b11110000
     if (first_byte & one_byte_mask) == 0:
-        return 1
+        return (1, first_byte & 0x3F)
     if (first_byte & two_byte_mask) == two_byte_mask:
-        return 2
+        return (2, first_byte & 0x1F)
     if (first_byte & three_byte_mask) == three_byte_mask:
-        return 3
+        return (3, first_byte & 0x0F)
     if (first_byte & four_byte_mask) == four_byte_mask:
-        return 4
-    return -1
+        return (4, first_byte & 0xF)
+    return (-1, 0)
+
+
+def extract_following_bits(following_byte):
+    """Extracts following bits"""
+    one_byte_mask = 0x80  # 0b10000000
+    following_bytes_mask = 0xC0  # 0b11000000
+    if (following_byte & following_bytes_mask) == one_byte_mask:
+        return (1, following_byte & 0x3F)
+    return (-1, 0)
 
 
 def validUTF8(data):
-    """Checks if all characters in data is valid UTF8
-    A valid UTF8 encoded data must meet these 3 criteria
-    1. If number of bytes returned by the initial bits of the first byte
-    is invalid i.e between 1 to 4 bytes and is invalid (check function
-    get_number_of_bytes() i.e (b10xxxxxx or b1100xxxx)
-    2. If the number of bytes in data is greater that the number of bytes
-    set in the bits of the first byte (Someone is lying :)
-    3. If the following bytes dont start with bits (1 and 0) b10xxxxxx
+    """Validates utf8 data
+    <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
     """
-    one_byte_mask = 0x80  # 0b10000000
-    following_bytes_mask = 0xC0  # 0b11000000
-    i = 0
     if len(data) == 0:
-        return False
+        False
+    i = 0
+    while i < len(data):
+        (n_bytes, _) = extract_and_count_bits(data[i])
 
-    n_bytes = get_number_of_bytes(data[i])
-    if n_bytes == -1:
-        return False
-
-    if i + n_bytes > len(data):
-        return False
-
-    for j in range(i + 1, i + n_bytes):
-        if (data[j] & following_bytes_mask) != one_byte_mask:
+        if n_bytes == -1:
             return False
+        # if n_bytes == 1:
+        #     continue
+        if i + n_bytes > len(data):
+            return False
+        for j in range(i + 1, n_bytes):
+            (current_byte_count, _) = extract_following_bits(data[j])
+            if current_byte_count == -1:
+                return False
+        i += n_bytes
     return True
