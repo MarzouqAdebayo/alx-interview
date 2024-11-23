4231f1ec3fa918f542c5b43edb670b643b311f81
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-23 13:51:14 +0100

    Completed task

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+const request = require("request");
+const API_URL = "https://swapi-api.hbtn.io/api";
+
+if (process.argv.length > 2) {
+  request(`${API_URL}/films/${process.argv[2]}`, function (error, _, body) {
+    if (error) {
+      console.log(error);
+    }
+    const charactersUrls = JSON.parse(body).characters;
+    const characters = charactersUrls.map(
+      (url) =>
+        new Promise((resolve, reject) => {
+          request(url, function (error, _, body) {
+            if (error) {
+              reject(error);
+            }
+            resolve(JSON.parse(body).name);
+          });
+        }),
+    );
+
+    Promise.all(characters)
+      .then((names) => console.log(names.join("\n")))
+      .catch((err) => console.log(err));
+  });
+}
