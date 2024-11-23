a7502552ecfe0596b1f6904fe9215757a0c5da58
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-23 13:52:57 +0100

    Lint

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const request = require("request");
-const API_URL = "https://swapi-api.hbtn.io/api";
+const request = require('request');
+const API_URL = 'https://swapi-api.hbtn.io/api';
 
 if (process.argv.length > 2) {
   request(`${API_URL}/films/${process.argv[2]}`, function (error, _, body) {
@@ -17,11 +17,11 @@ if (process.argv.length > 2) {
             }
             resolve(JSON.parse(body).name);
           });
-        }),
+        })
     );
 
     Promise.all(characters)
-      .then((names) => console.log(names.join("\n")))
+      .then((names) => console.log(names.join('\n')))
       .catch((err) => console.log(err));
   });
 }
