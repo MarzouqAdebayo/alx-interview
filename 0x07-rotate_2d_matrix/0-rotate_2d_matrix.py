986156888d868dc3712e0d97a8f329355527a979
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-23 12:48:44 +0100

    :bug: Added comments to help understand the code quickly.

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,6 +6,9 @@ def rotate_2d_matrix(matrix):
     n = len(matrix)
     # reverse the array and transpose
     matrix.reverse()
+    # loop thru each position and only swap values above the diagonal
+    # to prevent duplicate swapping
+    # swap [i][j] with [j][i]
     for i in range(n):
         for j in range(i + 1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
