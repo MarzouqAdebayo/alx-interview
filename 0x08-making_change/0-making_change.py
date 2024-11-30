7e81ea4ec2cff676016a02dab1152e7645fd06b9
MarzouqAdebayo <marzouqaadebayo@gmail.com>
2024-11-30 23:01:12 +0100

    Trying another method

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,7 +2,7 @@
 """Making Change module '0-making_change.py' """
 
 
-def makeChange(coins, sum):
+def makeChangeInEff(coins, sum):
     if sum <= 0:
         return 0
     table = {0: 0}
@@ -17,3 +17,24 @@ def makeChange(coins, sum):
     if table.get(sum) and (table[sum] <= 0 or table[sum] > sum):
         return -1
     return table[sum]
+
+
+def makeChange(coins, sum):
+    if sum <= 0 or len(coins) <= 0:
+        return 0
+    sorted_coins = sorted(coins)
+    n = len(sorted_coins)
+    rem = sum
+    no_of_change = 0
+    i = n - 1
+    while rem > 0:
+        if i > n - 1 or i < 0:
+            break
+        soln = rem // sorted_coins[i]
+        if soln >= 1:
+            no_of_change += soln
+            rem = rem - (soln * sorted_coins[i])
+        i -= 1
+    if rem != 0:
+        return -1
+    return no_of_change
